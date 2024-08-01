f82f1f655ad6d74890106be9333d6feb6a54f8a2
DimDrk <dimdrk@gmail.com>
2024-08-01 21:40:57 +0300

    insert, update added

--- src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOIml.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOIml.java
@@ -2,18 +2,63 @@ package gr.aueb.cf.schoolapp.dao;
 
 import gr.aueb.cf.schoolapp.dao.exceptions.TeacherDAOException;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.service.util.DBUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TeacherDAOIml implements ITeacherDAO {
+
     @Override
     public Teacher insert(Teacher teacher) throws TeacherDAOException {
-        return null;
+        String sql = "INSERT INTO teachers (firstname, lastname) VALUES (?, ?)";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            // extract model info
+            String firstname = teacher.getFirstname();
+            String lastname = teacher.getLastname();
+
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+
+            int n = ps.executeUpdate();
+            // logging
+            return teacher; //TBD
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in insert teacher: " + teacher);
+        }
     }
 
     @Override
     public Teacher update(Teacher teacher) throws TeacherDAOException {
-        return null;
+        String sql = "UPDATE teachers SET firstname = ?, lastname = ? WHERE id = ?)";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            // extract model info
+            int id = teacher.getId();
+            String firstname = teacher.getFirstname();
+            String lastname = teacher.getLastname();
+
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setInt(3, id);
+
+            int n = ps.executeUpdate();
+            // logging
+            return teacher; //TBD
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in update teacher: " + teacher);
+        }
     }
 
     @Override
